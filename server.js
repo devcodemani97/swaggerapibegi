3a778299c9163c034a821a096c844392b72aaa0c
devcodemani97 <devcodemani97@gmail.com>
2020-09-13 09:20:37 +0530

    random session id and ipaddress

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let getIP = require('ipware')().get_ip;
 
 let mongoose = require('mongoose'),
     Schema = mongoose.Schema;
-
+    var crypto = require('crypto');
 mongoose.connect('mongodb://localhost:27017/swagger-demo',
     {
         useNewUrlParser: true,
@@ -36,10 +36,15 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+var generate_key = function() {
+    return crypto.randomBytes(16).toString('base64');
+};
 //middleware for create
 let createemployee = function (req, res, next) {
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    res.setHeader('ip_address', ip);
+    res.setHeader('session_id', generate_key())
     let employees = new employee(req.body);
-
     employees.save(function (err) {
         if (err) {
             next(err);
@@ -53,7 +58,9 @@ let getOneemployee = function (req, res) {
 };
 
 let getByIdemployee = function (req, res, next, id) {
-
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    res.setHeader('ip_address', ip);
+    res.setHeader('session_id', generate_key())
     employee.findOne({ _id: id }, function (err, employee) {
         if (err) {
             next(err);
@@ -78,6 +85,6 @@ app.use('/api/v1', router);
 
 app.listen(3000,function(){
     console.log('server is running in 3000 port')
-    console.log(`For swagger ui use this link http://localhost:3000/api-docs`)
+    console.log(`For swagger ui use this link http://localhost:3000/api-doc`)
 });
 module.exports = app;
